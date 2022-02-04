38d09e4abc7bc2a1c6a0a91f6f30f9e082226bd6
Abdullah Ismailogullari <abdullahismailogullari@Abdullahs-Air.fritz.box>
2022-02-04 16:04:55 +0100

    added all requested conditions

--- src/main/java/de/neuefische/myCoolPassValidation/PassValidator.java
+++ src/main/java/de/neuefische/myCoolPassValidation/PassValidator.java
@@ -1,4 +1,67 @@
 package de.neuefische.myCoolPassValidation;
 
+import java.util.Locale;
+
 public class PassValidator {
+
+    public static boolean passValidation(String password){
+
+        if (password.length() < 8) {
+            System.out.println("Not a valid password: needs to contain 8 chars at least!");
+            return false;
+        }
+
+        boolean numberChecker = true;
+        while (numberChecker){
+            for (int i = 0; i < 10; i++) {
+                if (password.contains("" + i)) {
+                    numberChecker = false;
+                }
+            }
+            if (numberChecker){
+                System.out.println("Not a valid password: needs to contain one number at least");
+                return false;
+            }
+        }
+
+
+        if (password.toLowerCase() == password){
+            System.out.println("Not a valid password: needs to contain upper case letters");
+            return false;
+        }else if(password.toUpperCase() == password){
+            System.out.println("Not a valid password: needs to contain lower case letters");
+            return false;
+        }
+
+        boolean specialCharChecker = true;
+        while (specialCharChecker){
+            String[] specialChars = {"!", "%", "_", "?", "$", "&", "="};
+            for (int i = 0; i < specialChars.length; i++) {
+                if (password.contains(specialChars[i])) {
+                    specialCharChecker = false;
+                }
+            }
+            if (specialCharChecker){
+                System.out.println("Not a valid password: needs to contain at least one of the following special characters: ");
+                for (int i = 0; i < specialChars.length; i++){
+                    System.out.print(specialChars[i] + " ");
+                }
+                return false;
+            }
+        }
+
+        System.out.println("Password accepted");
+        return true;
+    }
+
+
+
+
+
+
+    public static void main(String[] args) {
+        passValidation("hadhH377hadk");
+    }
+
+
 }
--- src/test/java/de/neuefische/myCoolPassValidation/PassValidatorTest.java
+++ src/test/java/de/neuefische/myCoolPassValidation/PassValidatorTest.java
@@ -1,11 +1,55 @@
 package de.neuefische.myCoolPassValidation;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PassValidatorTest {
 
+    @Test
+    void shouldNotValidatePassOfLength0(){
+        String pass = "";
+        Assertions.assertFalse(PassValidator.passValidation(pass));
+    }
 
+    @Test
+    void shouldNotValidatePassWithNoNumbers(){
+        String pass = "bcdefghijk";
+        Assertions.assertFalse(PassValidator.passValidation(pass));
+    }
+
+    @Test
+    void shouldNotValidatePassWithOnlyLowerCase(){
+        String pass = "8dadkh83uuuuu";
+        Assertions.assertFalse(PassValidator.passValidation(pass));
+    }
+
+    @Test
+    void shouldNotValidatePassWithOnlyUpperCase(){
+        String pass = "8ASTGDBH77";
+        Assertions.assertFalse(PassValidator.passValidation(pass));
+    }
+
+    @Test
+    void shouldNotValidatePassWithNoSpecialChars(){
+        String pass = "djadHdw6766";
+        Assertions.assertFalse(PassValidator.passValidation(pass));
+    }
+
+    @Test
+    void shouldValidatePassWith8Chars(){
+        String pass = "7dgHiwo!";
+        Assertions.assertTrue(PassValidator.passValidation(pass));
+    }
+
+    /*
+    @Test
+    void shouldValidatePassWithNumbers(){
+        String pass = "sfksjhsfbbs";
+        Assertions.assertFalse(PassValidator.passValidation(pass));
+    }
+
+     */
 
 }
