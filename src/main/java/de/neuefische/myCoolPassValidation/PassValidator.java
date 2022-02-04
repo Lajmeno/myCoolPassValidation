053aaebf11f5b06e1743168000f74a5c559d9087
Abdullah Ismailogullari <abdullahismailogullari@Abdullahs-Air.fritz.box>
2022-02-04 13:50:48 +0100

    first Commit

---
+++ src/main/java/de/neuefische/myCoolPassValidation/PassValidator.java
@@ -0,0 +1,4 @@
+package de.neuefische.myCoolPassValidation;
+
+public class PassValidator {
+}
---
+++ src/test/java/de/neuefische/myCoolPassValidation/PassValidatorTest.java
@@ -0,0 +1,11 @@
+package de.neuefische.myCoolPassValidation;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PassValidatorTest {
+
+
+
+}
